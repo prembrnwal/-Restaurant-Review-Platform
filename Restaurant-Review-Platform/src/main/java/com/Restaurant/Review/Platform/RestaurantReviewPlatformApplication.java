0ade714e68dca30e8179aceaa3066fb73aa01412
Prem__brnwal <premburnwal31@gmail.com>
2026-03-18 23:51:36 +0530

    initialisation of project

---
+++ Restaurant-Review-Platform/src/main/java/com/Restaurant/Review/Platform/RestaurantReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Restaurant.Review.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewPlatformApplication.class, args);
+	}
+
+}
---
+++ Restaurant-Review-Platform/src/test/java/com/Restaurant/Review/Platform/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Restaurant.Review.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
